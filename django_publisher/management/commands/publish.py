9d3cd2792c243b375683978944b84a981c735dac
Allen, Timothy <tallen@wharton.upenn.edu>
2017-11-26 01:51:33 +0000

    First round done. Need to switch to os.path.join, but good enough for tonight.

--- django_publisher/management/commands/publish.py
+++ django_publisher/management/commands/publish.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
@@ -27,23 +30,35 @@ class Command(BaseCommand):
             default=False,
             help='''Shuts off not quiet output.'''
         )
+        parser.add_argument(
+            '--stamp',
+            action='store',
+            dest='stamp',
+            default='{0:%Y-%m-%d-%H-%M-%S}'.format(datetime.datetime.now()),
+            help='''Shuts off not quiet output.'''
+        )
 
     def command_output(self, stdout, stderr, quiet):
+        """
+        Dumps the output of the SSH command run via paramiko and
+        error, if applicable.
+
+        Output the errors, even in quiet mode.
+        """
         if not quiet:
             print(stdout.read().decode("utf-8"))
-            print(stderr.read().decode("utf-8"))
         else:
             stdout.read()
+        print(stderr.read().decode("utf-8"))
 
     def handle(self, *args, **options):
         """
-        Handler for the command. Backs up the database in the source environment,
-        then restores it to the target. Sanity check in place to never restore to
-        production.
+        Gets the appropriate settings from Django and publishes the repository
+        to the target servers for the instance selected.
         """
-
-        # Grab the quiet settings
+        # Grab the quiet settings and unique stamp
         quiet = options['quiet']
+        stamp = options['stamp']
 
         # Check to ensure the require setting is in Django's settings.
         if hasattr(settings, 'DJANGO_PUBLISHER_INSTANCES'):
@@ -79,85 +94,66 @@ class Command(BaseCommand):
 
             ssh = SSHClient()
             ssh.set_missing_host_key_policy(AutoAddPolicy())
-            print(server)
-            print(instance['server_user'])
             ssh.connect(server, username=instance['server_user'])
 
             stdin, stdout, stderr = ssh.exec_command(
                 """
                 cd {code_path}
-                rm -rf {name}-{branch}-new
-                git clone --verbose -b {branch} {repository} {name}-{branch}-new
+                git clone --verbose -b {branch} {repository} {name}-{branch}-{stamp}
+                ln -sfn {name}-{branch}-{stamp} {name}-{branch}
                 """.format(
                     code_path=instance['code_path'],
                     name=instance['name'],
                     branch=instance['branch'],
                     repository=instance['repository'],
-                )
-            )
-            print(
-                """
-                cd {code_path}
-                rm -rf {name}-{branch}-new
-                git clone --verbose -b {branch} {repository} {name}-{branch}-new
-                """.format(
-                    code_path=instance['code_path'],
-                    name=instance['name'],
-                    branch=instance['branch'],
-                    repository=instance['repository'],
+                    stamp=stamp,
                 )
             )
             self.command_output(stdout, stderr, quiet)
 
-            import sys
-            sys.exit()
+            print("Installing requirements in a new virtualenv, collecting static files, and running migrations...")
+            stdin, stdout, stderr = ssh.exec_command(
+                """
+                cd {virtualenv_path}
+                virtualenv --python={virtualenv_python_path} {name}-{branch}-{stamp}
+                . {name}-{branch}-{stamp}/bin/activate
+                cd {install_path}
+                pip install --ignore-installed -r {requirements}
+                python manage.py collectstatic --noinput --settings={settings}
+                python manage.py migrate --noinput --settings={settings}
+                """.format(
+                    virtualenv_path=instance['virtualenv_path'],
+                    virtualenv_python_path=instance['virtualenv_python_path'],
+                    name=instance['name'],
+                    branch=instance['branch'],
+                    stamp=stamp,
+                    install_path=os.path.join(
+                        instance['code_path'],
+                        '{name}-{branch}-{stamp}'.format(
+                            name=instance['name'],
+                            branch=instance['branch'],
+                            stamp=stamp,
+                        ),
+                    ),
+                    requirements=instance['requirements'],
+                    settings=instance['settings'],
+                )
+            )
+            self.command_output(stdout, stderr, quiet)
 
-            print("Installing requirements in a new virtualenv...")
-            stdin, stdout, stderr = ssh.exec_command("""
-                rmvirtualenv {1}-{0}-new
-                mkvirtualenv {1}-{0}-new
-                pip install --ignore-installed -r /var/django/html/{1}-{0}-new/requirements/{0}.txt
-            """.format(instance, instance['name']))
-            if not quiet:
-                print(stdout.read().decode("utf-8"))
-            else:
-                stdout.read()
-            print(stderr.read().decode("utf-8"))
-
-            print("Collecting static files...")
-            stdin, stdout, stderr = ssh.exec_command("""
-                cd /var/django/html/{1}-{0}-new
-                workon {1}-{0}-new
-                python manage.py collectstatic --noinput --settings=config.settings.{0}
-            """.format(instance, instance['name']))
-            if not quiet:
-                print(stdout.read().decode("utf-8"))
-            else:
-                stdout.read()
-            print(stderr.read().decode("utf-8"))
-
-            print("Saving previous two publishes, and installing new...")
-            stdin, stdout, stderr = ssh.exec_command("""
-                rmvirtualenv {1}-{0}-old-old
-                cpvirtualenv {1}-{0}-old {1}-{0}-old-old
-                rmvirtualenv {1}-{0}-old
-                cpvirtualenv {1}-{0} {1}-{0}-old
-                rmvirtualenv {1}-{0}
-                cpvirtualenv {1}-{0}-new {1}-{0}
-                rmvirtualenv {1}-{0}-new
-                cd /var/django/html
-                rm -rf {1}-{0}-old-old
-                mv {1}-{0}-old {1}-{0}-old-old
-                mv {1}-{0} {1}-{0}-old
-                mv {1}-{0}-new {1}-{0}
-                workon {1}-{0}
-                cd {1}-{0}
-                python manage.py migrate --noinput --settings=config.settings.{0}
-            """.format(instance, instance['name']))
-            if not quiet:
-                print(stdout.read().decode("utf-8"))
-            else:
-                stdout.read()
-            print(stderr.read().decode("utf-8"))
+            print("Updating the code and virtualenv symlinks for the new publish...")
+            stdin, stdout, stderr = ssh.exec_command(
+                """
+                ln -sfn {code_path}/{name}-{branch}-{stamp} {code_path}/{name}-{branch}
+                ln -sfn {virtualenv_path}/{name}-{branch}-{stamp} {virtualenv_path}/{name}-{branch}
+                """.format(
+                    code_path=instance['code_path'],
+                    name=instance['name'],
+                    branch=instance['branch'],
+                    stamp=stamp,
+                    virtualenv_path=instance['virtualenv_path'],
+                )
+            )
+            self.command_output(stdout, stderr, quiet)
 
         print("All done!")
