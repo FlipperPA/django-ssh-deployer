031e92028adcfc3d2f17a099b9ebe4829b4fa71c
Allen, Timothy <tallen@wharton.upenn.edu>
2020-05-16 07:43:37 -0400

    Paint it black.

--- setup.py
+++ setup.py
@@ -1,35 +1,32 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
-    name='django-ssh-deployer',
+    name="django-ssh-deployer",
     version="0.4.5",
-    description='This package provides Django management commands to deploy your site over SSH via Paramiko.',
+    description="This package provides Django management commands to deploy your site over SSH via Paramiko.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Tim Allen',
-    author_email='tim@pyphilly.org',
-    url='https://github.com/FlipperPA/django-ssh-deployer',
+    long_description_content_type="text/markdown",
+    author="Tim Allen",
+    author_email="tim@pyphilly.org",
+    url="https://github.com/FlipperPA/django-ssh-deployer",
     include_package_data=True,
     packages=find_packages(),
     zip_safe=False,
-    install_requires=[
-        'Django',
-        'paramiko',
-    ],
+    install_requires=["Django", "paramiko",],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Framework :: Django',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
 )
